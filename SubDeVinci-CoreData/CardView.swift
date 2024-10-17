edf8900ab5f242ac0c96c32c26312fcf119b316a
By-Kvn <kevin.labatte0@gmail.com>
2024-10-17 18:44:05 +0200

    Last version, add movie blind test + clean code & file

--- SubDeVinci-CoreData/CardView.swift
+++ SubDeVinci-CoreData/CardView.swift
@@ -1,18 +1,18 @@
 //
-//  CombatView.swift
+//  CardView.swift
 //  SubDeVinci-CoreData
 //
-//  Created by Kevin Labatte on 16/10/2024.
+//  Created by Kevin Labatte on 17/10/2024.
 //
 
 import SwiftUI
 
-struct CombatView: View {
+struct CardView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    CombatView()
+    CardView()
 }
